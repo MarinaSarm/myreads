e2969f4c441552716e83d45410ed05092acc124e
MarinaSarm <marsarman@yandex.ru>
2018-07-22 17:50:53 +0200

    move more html into ListBooks component

--- src/App.js
+++ src/App.js
@@ -103,33 +103,19 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
+            <div className="list-books-content">      
                     <ListBooks
+                      shelfTitle="Currently Reading"
                       onMoveBook={this.moveBook}
                       books={this.state.booksArray} />
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
                     <ListBooks
+                      shelfTitle="Want to Read"
                       onMoveBook={this.moveBook}
                       books={this.state.booksArray} />
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
                     <ListBooks
+                      shelfTitle="Read"
                       onMoveBook={this.moveBook}
                       books={this.state.booksArray} />
-                  </div>
-                </div>
-              </div>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,28 +8,33 @@ class ListBooks extends Component {
   }
   render() {
     return(
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.coverURL})` }}></div>
-                <div className="book-shelf-changer">
-                  <select onChange={(event) => this.props.onMoveBook(book, event)}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
-          </li>
-        ))}
-      </ol>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books.map((book) => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.coverURL})` }}></div>
+                      <div className="book-shelf-changer">
+                        <select onChange={(event) => this.props.onMoveBook(book, event)}>
+                          <option value="move" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
     )
   }
 }
