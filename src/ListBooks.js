d5fbb5df8496fae8f41a8985b342c6e942a8316e
MarinaSarm <marsarman@yandex.ru>
2018-07-22 18:26:58 +0200

    grab books from booksAPI

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,21 +3,21 @@ import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
   static propTypes = {
-    shelfTitle: PropTypes.string.isRequired,
+    shelf: PropTypes.string.isRequired,
     books: PropTypes.array.isRequired,
     onMoveBook: PropTypes.func.isRequired
   }
   render() {
     return(
         <div className="bookshelf">
-          <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+          <h2 className="bookshelf-title">{this.props.shelf}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              {this.props.books.filter((book) => book.bookShelf === this.props.shelfTitle).map((book) => (
+              {this.props.books.filter((book) => book.shelf === this.props.shelf).map((book) => (
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.coverURL})` }}></div>
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                       <div className="book-shelf-changer">
                         <select onChange={(event) => this.props.onMoveBook(book, event)}>
                           <option value="move" disabled>Move to...</option>
