fa737ea5fdf4e3369f509702b31e146afd1900e4
MarinaSarm <marsarman@yandex.ru>
2018-07-23 18:17:10 +0200

    set up BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
@@ -14,14 +15,17 @@ class BooksApp extends React.Component {
     booksArray: [],
     showSearchPage: false
   }
+  /**
+  *grab books from BooksAPI
+  */
   componentDidMount() {
     BooksAPI.getAll().then((booksArray) => {
       this.setState({ booksArray })
     })
   }
   moveBook = (book, event) => {
-    let movedBook = this.state.booksArray.filter( (b) => b.id === book.id)
-    let targetShelf = event.target.value
+    const movedBook = this.state.booksArray.filter( (b) => b.id === book.id)
+    const targetShelf = event.target.value
     if (targetShelf !== "none") {
       movedBook[0].shelf = targetShelf
       this.setState((state) => ({
@@ -38,26 +42,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
